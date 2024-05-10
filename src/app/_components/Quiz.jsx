55407c0cf2e20ca65c610383d82b3664ba13ec69
Vats4065 <vatsal.r.lakhani2626@gmail.com>
2024-05-10 18:28:26 +0530

    delete

--- src/app/_components/Quiz.jsx
+++ src/app/_components/Quiz.jsx
@@ -1,25 +1,43 @@
 import React, { useEffect, useState } from "react";
 
+const questionOption = {
+  questionOption: "",
+  selected: false,
+};
+
 const defaultVal = {
   question: "",
   btn: "single",
-  ansOptions: [""],
+  ansOptions: [questionOption],
+  len: true,
 };
+
 function Quiz() {
-  const [mul, setMul] = useState(false);
+  const [val, setVal] = useState({
+    question: "",
+    btn: "single",
+    ansOptions: [questionOption],
+    len: true,
+  });
+
   const [que, setQue] = useState([]);
   const [mainQue, setMainQue] = useState([]);
   console.log(mainQue, "mainqueuue");
-  const [len, setLen] = useState(true);
+
   const [radio, setRadio] = useState(defaultVal.btn);
 
   console.log(radio);
 
   useEffect(() => {
-    setQue([...que, Number(...(que + 1))]);
+    setQue([...que, Number(...que)]);
     setMainQue([...mainQue, defaultVal]);
     console.log("ased", que);
+    console.log("ghudfghudfsghusdfghu", mainQue);
   }, []);
+
+  useEffect(() => {
+    console.log({ mainQue });
+  }, [mainQue]);
 
   const handleMain = (e) => {
     e.preventDefault();
@@ -33,30 +51,133 @@ function Quiz() {
     // setQue([...que, Number(...(que + 1))]);
     // console.log("bth", que);
 
-    console.log(mainQue[mainQue.length - 1].ansOptions.length);
+    console.log("1111", mainQue[mainQue.length - 1]);
     console.log(mainQue);
 
-    setRadio(mainQue[mainQue.length - 1].ansOptions.length);
+    // setRadio(mainQue[mainQue.length - 1].ansOptions.length);
+    // console.log("loploop", mainQue[index].ansOptions.length == 5);
 
-    if (que.length == 4) {
-      setLen(false);
-    }
     const newQueue = [...mainQue]?.map((que, i) => {
       if (index === i) {
-        return {
-          ...que,
-          ansOptions: [...que.ansOptions, ""],
-        };
+        if (mainQue[index].ansOptions.length === 5) {
+          return {
+            ...que,
+            len: false,
+          };
+        } else {
+          return {
+            ...que,
+            ansOptions: [...que.ansOptions, questionOption],
+          };
+        }
       }
       return que;
     });
     setMainQue([...newQueue]);
+    console.log(newQueue);
+  };
+
+  const handleChangeRadioButton = (e, index) => {
+    if (e.target.value == "multiple") {
+      setRadio("multiple");
+    } else {
+      setRadio("single");
+    }
+    const newQueue = mainQue?.map((que, i) => {
+      console.log(i === index, "indexxx");
+      if (i === index) {
+        que.btn = e.target.value;
+      }
+      console.log(que, "queeeee");
+      return que;
+    });
+    setMainQue(newQueue);
+  };
+
+  const handleDelete = (index) => {
+    const temp = [...mainQue];
+    temp.splice(index, 1);
+    setMainQue(temp);
+  };
+
+  // const handleChildDelete = (index,ind) => {
+  //   const temp = [...mainQue];
+  //   const del = temp[index].ansOptions[ind]
+  //   console.log(del);
+  //   del.splice(ind,1);
+  //   console.log(temp);
+  //   console.log(del,"454454444555");
+  //   // setMainQue(...mainQue,
+  //   //   del);
+  // };
+
+  const handleChildDelete = (mainInd, ansInd) => {
+    const temp = [...mainQue];
+    const del = temp[mainInd].ansOptions;
+    // console.log(del);
+    // const newArray = del.filter((item,index)=>index !== ind)
+    // console.log(newArray);
+
+    const onChangeDtaa = [...mainQue]?.map((item, index) => {
+      if (index === mainInd) {
+        return {
+          ...item,
+          ansOptions: del.filter((item, index) => index !== ansInd),
+          // ansOptions: [...item?.ansOptions]?.map((option, i) => {
+          //   if (i === ansInd) {
+          //     return {
+          //       ...option,
+          //       questionOption: e?.target?.value,
+          //     };
+          //   }
+          //   return option;
+          // }),
+        };
+      }
+      return item;
+    });
+    setMainQue(onChangeDtaa);
+  };
+
+  const handleData = (e, index) => {
+    const newQueue = [...mainQue]?.map((que, i) => {
+      if (index === i) {
+        return {
+          ...que,
+          question: e.target.value,
+        };
+      }
+      return que;
+    });
+    setMainQue(newQueue);
+  };
+
+  const handleAnswerOption = (e, mainInd, ansInd) => {
+    const onChangeDtaa = [...mainQue]?.map((item, index) => {
+      if (index === mainInd) {
+        return {
+          ...item,
+          ansOptions: [...item?.ansOptions]?.map((option, i) => {
+            if (i === ansInd) {
+              return {
+                ...option,
+                questionOption: e?.target?.value,
+              };
+            }
+            return option;
+          }),
+        };
+      }
+      return item;
+    });
+    setMainQue(onChangeDtaa);
+    // console.log(onChangeDtaa, "sdhasdjhasdjhasdasjhd");
   };
   return (
     <>
       <section className="bg-light py-5">
         <div className="container">
-          <form action="" className="w-50 m-auto ">
+          <form action="" className="w-70 m-auto ">
             <div className="form-group">
               <label>Title</label>
               <input
@@ -65,6 +186,7 @@ function Quiz() {
                 id="exampleInputEmail1"
                 placeholder="Enter title"
                 name="title"
+                // value={}
               />
             </div>
 
@@ -89,122 +211,154 @@ function Quiz() {
               />
             </div>
 
-            {mainQue.map((item, index) => {
+            {mainQue?.map((item, index) => {
               console.log("jixfhf", index);
+              console.log(item.ansOptions, "147852369582");
+
               return (
                 <div key={index}>
-                  <div className="mt-5 d-flex justify-content-between align-items-center">
+                  <div className="main border border-2  mt-5 p-5">
                     <div className="form-group ">
-                      <label>Question No.{index + 1}</label>
+                      <h3>Question No.{index + 1}</h3>
                       <input
                         type="text"
                         className="form-control "
                         id="exampleInputEmail1"
                         placeholder="Enter question"
                         name="question"
+                        value={item?.question}
+                        onChange={(e) => handleData(e, index)}
                       />
                     </div>
-                    <div className="form-group mt-4">
-                      <label>Question Type </label>
-                      <select
-                        className="custom-select"
-                        name="question_type"
-                        onChange={(e) => {
-                          if (e.target.value == "multiple") {
-                            setRadio("multiple");
-                          } else {
-                            setMainQue([
-                              ...mainQue,
-                              (mainQue[index].btn = "single"),
-                            ]);
-                            setRadio("single");
-                          }
-                          console.log("ssas", radio);
-                        }}
-                      >
-                        <option value="single" defaultValue>
-                          Single
-                        </option>
-                        <option value="multiple">Multiple</option>
-                      </select>
-                    </div>
-                  </div>
+                    <div className="mt-5 d-flex justify-content-between align-items-center">
+                      <div className="form-group mt-4">
+                        <label>Question Type </label>
+                        <select
+                          className="custom-select"
+                          name="question_type"
+                          onChange={(e) => handleChangeRadioButton(e, index)}
+                        >
+                          <option value="single">Single</option>
+                          <option value="multiple">Multiple</option>
+                        </select>
+                      </div>
 
-                  {item?.ansOptions?.map((item, ind) => {
-                    console.log("hvg", ind);
-                    console.log("dsdsdsd", index);
-                    // console.log(findIndex);
-                    console.log(
-                      "jinhuiwrgwrg",
-                      typeof mainQue[mainQue.length - 1].btn
-                    );
-                    return (
-                      <div key={ind}>
-                        <div className="d-flex justify-content-between align-items-center mt-5 ">
-                          <div>
-                            <input
-                              className="form-control"
-                              type="text"
-                              value=""
-                            />
-                            <label
-                              className="form-label"
-                              htmlFor="defaultCheck1"
-                            >
-                              Answer{ind + 1}
-                            </label>
-                          </div>
-                          {mainQue[index].btn == "multiple" ? (
-                            <div className="" style={{ marginLeft: "240px" }}>
+                      <div>
+                        {index > 0 ? (
+                          <button
+                            className="btn btn-outline-danger mt-4"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              handleDelete(index);
+                            }}
+                          >
+                            Delete Question
+                          </button>
+                        ) : (
+                          ""
+                        )}
+                      </div>
+                    </div>
+
+                    {item?.ansOptions?.map((ele, ind) => {
+                      // debugger;
+                      console.log(ele, "65465146");
+                      console.log("hvg", ind);
+                      console.log("dsdsdsd", index);
+                      // console.log(findIndex);
+                      console.log(
+                        "jinhuiwrgwrg",
+                        typeof mainQue[mainQue.length - 1].btn
+                      );
+                      return (
+                        <div key={ind}>
+                          <div className="d-flex justify-content-between align-items-center mt-5 ">
+                            <div>
                               <input
-                                className="form-check-input"
-                                type="checkbox"
-                                value=""
-                                id="defaultCheck1"
+                                className="form-control"
+                                type="text"
+                                onChange={(e) => {
+                                  handleAnswerOption(e, index, ind);
+                                }}
+                                placeholder="Add Answer Option"
+                                name="ansOptions"
+                                value={ele?.questionOption}
                               />
                               <label
-                                className="form-check-label ms-1"
+                                className="form-label"
                                 htmlFor="defaultCheck1"
                               >
-                                Select Answer
+                                Answer{ind + 1}
                               </label>
                             </div>
-                          ) : (
-                            <>
-                              <div className="form-check">
+                            {mainQue[index].btn == "multiple" ? (
+                              <div className="" style={{ marginLeft: "240px" }}>
                                 <input
                                   className="form-check-input"
-                                  type="radio"
-                                  name={`name_${index}`}
-                                  id="exampleRadios1"
-                                  value="opation 1"
+                                  type="checkbox"
+                                  value=""
+                                  id="defaultCheck1"
                                 />
                                 <label
-                                  className="form-check-label"
-                                  htmlFor="exampleRadios1"
+                                  className="form-check-label ms-1"
+                                  htmlFor="defaultCheck1"
                                 >
                                   Select Answer
                                 </label>
                               </div>
-                            </>
-                          )}
-                        </div>
-                      </div>
-                    );
-                  })}
+                            ) : (
+                              <>
+                                <div className="form-check">
+                                  <input
+                                    className="form-check-input"
+                                    type="radio"
+                                    name={`name_${index}`}
+                                    id="exampleRadios1"
+                                    value="opation 1"
+                                  />
+                                  <label
+                                    className="form-check-label"
+                                    htmlFor="exampleRadios1"
+                                  >
+                                    Select Answer
+                                  </label>
+                                </div>
+                              </>
+                            )}
+                            {ind != 0 ? (
+                              <>
+                                <button
+                                  className="btn btn-outline-danger "
+                                  onClick={(e) => {
+                                    e.preventDefault();
 
-                  {len ? (
-                    <>
-                      <button
-                        className="btn btn-outline-primary mt-3"
-                        onClick={(e) => handleclick(e, index)}
-                      >
-                        Add New Option
-                      </button>
-                    </>
-                  ) : (
-                    ""
-                  )}
+                                    handleChildDelete(index, ind);
+                                  }}
+                                >
+                                  Delete
+                                </button>
+                              </>
+                            ) : (
+                              ""
+                            )}
+                          </div>
+                        </div>
+                      );
+                    })}
+
+                    {mainQue[index].len == true ? (
+                      <>
+                        <button
+                          className="btn btn-outline-primary mt-3"
+                          onClick={(e) => handleclick(e, index)}
+                        >
+                          Add New Option
+                        </button>
+                      </>
+                    ) : (
+                      ""
+                    )}
+                  </div>
                 </div>
               );
             })}
